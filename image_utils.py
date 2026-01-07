4a7ed38679865f28a4a9aa7454d3852702469d6b
naamabtov <naamabtov@gmail.com>
2026-01-07 13:00:34 +0200

    Refactor edge detection and image loading functions

--- image_utils.py
+++ image_utils.py
@@ -1,34 +1,31 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
 from scipy.signal import convolve2d
-
 
 def load_image(path):
-    image = image.open(path)
+    image = Image.open(path).convert("RGB")
     return np.array(image)
 
-
-    import numpy as np
-from scipy.signal import convolve2d
-
 def edge_detection(image):
-    gray = image.mean(ןimage,axis=2)
-    filter_vertical = np.array([
+    gray = image.mean(axis=2)
+
+    vertical_filter = np.array([
         [-1, 0, 1],
         [-2, 0, 2],
         [-1, 0, 1]
     ])
 
-    filter_horizontal = np.array([
+    horizontal_filter = np.array([
         [-1, -2, -1],
         [ 0,  0,  0],
         [ 1,  2,  1]
     ])
-    edgeX = convolve2d(gray, horizontal_filter,mode="same")
 
-    edgeY = convolve2d(gray, vertical_filter,mode="same")
+    edgeX = convolve2d(gray, horizontal_filter,
+                       mode="same", boundary="fill", fillvalue=0)
 
-    # Edge magnitude
+    edgeY = convolve2d(gray, vertical_filter,
+                       mode="same", boundary="fill", fillvalue=0)
+
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
-
     return edgeMAG
